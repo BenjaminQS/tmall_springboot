0803b095a6728672a527e27af9fb33b461350d20
BenjaminQS <donmingjie@126.com>
2019-01-02 23:14:52 +0800

    add pv

--- src/main/java/com/how2java/tmall/service/PropertyService.java
+++ src/main/java/com/how2java/tmall/service/PropertyService.java
@@ -1,5 +1,7 @@
 package com.how2java.tmall.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -15,37 +17,39 @@ import com.how2java.tmall.util.Page4Navigator;
 @Service
 public class PropertyService {
 
-	@Autowired
-	PropertyDAO propertyDAO;
-	
-	@Autowired
-	CategoryService categoryService;
-	
-	public void add(Property bean){
+	@Autowired PropertyDAO propertyDAO;
+	@Autowired CategoryService categoryService;
+
+	public void add(Property bean) {
 		propertyDAO.save(bean);
 	}
-	
+
 	public void delete(int id) {
 		propertyDAO.delete(id);
 	}
-	
+
 	public Property get(int id) {
 		return propertyDAO.findOne(id);
 	}
-	
+
 	public void update(Property bean) {
 		propertyDAO.save(bean);
 	}
-	
-	public Page4Navigator<Property> list(int cid, int start, int size, int navigatePages) {
-		Category category = categoryService.get(cid);
-		Sort sort = new Sort(Sort.Direction.DESC, "id");
-		Pageable pageable = new PageRequest(start, size, sort);
-		Page<Property> pageFromJPA = propertyDAO.findByCategory(category, pageable);
+
+	public Page4Navigator<Property> list(int cid, int start, int size,int navigatePages) {
+    	Category category = categoryService.get(cid);
 		
-		return new Page4Navigator<>(pageFromJPA, navigatePages);
+    	Sort sort = new Sort(Sort.Direction.DESC, "id");
+    	Pageable pageable = new PageRequest(start, size, sort);    	
+    	
+    	Page<Property> pageFromJPA =propertyDAO.findByCategory(category,pageable);
+    	
+    	return new Page4Navigator<>(pageFromJPA,navigatePages);
+    	
 	}
-	
-	
-	
+
+	public List<Property> listByCategory(Category category){
+		return propertyDAO.findByCategory(category);
+	}
+
 }
